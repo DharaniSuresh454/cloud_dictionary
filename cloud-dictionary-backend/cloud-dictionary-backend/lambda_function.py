65db2920db57985a6d9b2d2226a8169f20fca58d
unknown <dharani.suresh454@gmail.com>
2025-06-19 17:53:20 +0530

    Added lambda_function.py

--- cloud-dictionary-backend/cloud-dictionary-backend/lambda_function.py
+++ cloud-dictionary-backend/cloud-dictionary-backend/lambda_function.py
@@ -5,18 +5,38 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('DictionaryTable')
 
 def lambda_handler(event, context):
-    term = event['queryStringParameters']['term']
+    try:
+        term = event['queryStringParameters']['term']
+    except (KeyError, TypeError):
+        return {
+            'statusCode': 400,
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+            },
+            'body': json.dumps({'error': 'Missing query parameter: term'})
+        }
+
     response = table.get_item(Key={'term': term})
 
     if 'Item' in response:
         return {
             'statusCode': 200,
-            'headers': { "Access-Control-Allow-Origin": "*" },
-            'body': json.dumps(response['Item'])
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+            },
+            'body': json.dumps({
+                'term': response['Item'].get('term', ''),
+                'description': response['Item'].get('description', 'No description available.')
+            })
         }
     else:
         return {
             'statusCode': 404,
-            'headers': { "Access-Control-Allow-Origin": "*" },
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+            },
             'body': json.dumps({'error': 'Term not found'})
         }
